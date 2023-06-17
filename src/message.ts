501720ddca039726363351c085c28a1c57647def
ryankert01 <ryan980053@gmail.com>
2023-06-18 00:02:10 +0800

    add

--- src/message.ts
+++ src/message.ts
@@ -1,9 +1,71 @@
 function getEvacuationMessage(suggestions: any):any {
     return {
-        type: "text",
-        text: `We suggest you to go to one of the following 3 station:
-[1] ${suggestions[0]}`
-    };
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+          "type": "carousel",
+          "columns": [
+            {
+              "thumbnailImageUrl": "https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/1-1.png",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              },
+              "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=111"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=111"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/111"
+                }
+              ]
+            },
+            {
+              "thumbnailImageUrl": "https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/1-1.png",
+              "imageBackgroundColor": "#000000",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/222"
+              },
+              "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=222"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=222"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/222"
+                }
+              ]
+            }
+          ],
+          "imageAspectRatio": "rectangle",
+          "imageSize": "cover"
+        }
+      };
 }
 
 function getDangerousAreaMessage(dangerous_areas: any):any {
