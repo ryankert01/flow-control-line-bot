8ebd3b5524ff504e4f614d8e99ff06a369148d3a
Ryan <91534261+ryankert01@users.noreply.github.com>
2023-07-02 23:11:14 +0800

    uri encoding

--- src/message.ts
+++ src/message.ts
@@ -194,11 +194,11 @@ function getChoosePlaceMapMessage(prefered_place:number, choose_place_id: number
         "type": "image_carousel",
         "columns": [
           {
-            "imageUrl": `https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/${prefered_place}-${choose_place_id}.png`,
+            "imageUrl": encodeURI(`https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/${prefered_place}-${choose_place_id}.png`),
             "action": {
               "type": "uri",
               "label": "Navigate",
-              "uri": `https://www.google.com/maps/dir/?api=1&origin=%E7%9B%AE%E5%89%8D%E6%89%80%E5%9C%A8%E4%BD%8D%E7%BD%AE&destination=${choose_place_name}&travelmode=walking&dir_action=navigate`
+              "uri": encodeURI(`https://www.google.com/maps/dir/?api=1&origin=%E7%9B%AE%E5%89%8D%E6%89%80%E5%9C%A8%E4%BD%8D%E7%BD%AE&destination=${choose_place_name}&travelmode=walking&dir_action=navigate`)
             }
           }
         ]
