73063481596860f9f9abc0d572c50ba6dcd9c997
jhaoen <93873483+jhaoen@users.noreply.github.com>
2023-07-02 22:47:33 +0800

    update id in message.ts

--- src/message.ts
+++ src/message.ts
@@ -6,47 +6,57 @@ function getEvacuationMessage(suggestions: any):any {
     switch(suggestions[i]){
       case "台北101/世貿捷運站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/101%20MRT%20station.png");// station picture url
-        evac_indexs.push(0); // push station id
+        evac_indexs.push(1); // push station id
         break;
       }
       case "台北君悅酒店巴士站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E5%90%9B%E6%82%85%E9%85%92%E5%BA%97%E5%B7%B4%E5%A3%AB%E7%AB%99.png");
+        evac_indexs.push(2);
         break;
       }
       case "101國際購物中心公車站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/101%E5%9C%8B%E9%9A%9B%E8%B3%BC%E7%89%A9%E4%B8%AD%E5%BF%83.png");
+        evac_indexs.push(3);
         break;
       }
       case "台北101公車站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E5%8F%B0%E5%8C%97101%E5%85%AC%E8%BB%8A%E7%AB%99.png");
+        evac_indexs.push(4);
         break;
       }
       case "象山捷運站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E8%B1%A1%E5%B1%B1%E6%8D%B7%E9%81%8B%E7%AB%99.png");
+        evac_indexs.push(5);
         break;
       }
       case "世貿中心(莊敬)公車":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E4%B8%96%E8%B2%BF%E4%B8%AD%E5%BF%83%E8%8E%8A%E6%95%AC%E5%85%AC%E8%BB%8A.png");// station picture url
+        evac_indexs.push(6);
         break;
       }
       case "市政府捷運站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E5%B8%82%E6%94%BF%E5%BA%9C%E6%8D%B7%E9%81%8B%E7%AB%99.png");
+        evac_indexs.push(7);
         break;
       }
       case "興雅國中公車站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E8%88%88%E9%9B%85%E5%9C%8B%E4%B8%AD%E5%85%AC%E8%BB%8A%E7%AB%99.png");
+        evac_indexs.push(8);
         break;
       }
       case "世貿中心(基隆路)公車站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E4%B8%96%E8%B2%BF%E5%9F%BA%E9%9A%86%E8%B7%AF%E5%85%AC%E8%BB%8A%E7%AB%99.png");
+        evac_indexs.push(9);
         break;
       }
       case "松壽路口公車站":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/%E6%9D%BE%E5%A3%BD%E8%B7%AF%E5%8F%A3%E5%85%AC%E8%BB%8A%E7%AB%99.png");
+        evac_indexs.push(10);
         break;
       }
       case "YouBike信義廣場":{
         evac_image.push("https://raw.githubusercontent.com/ryankert01/flow-control-line-bot/main/map_pics/Youbike%E4%BF%A1%E7%BE%A9%E5%BB%A3%E5%A0%B4.png");
+        evac_indexs.push(11);
         break;
       }
     }
